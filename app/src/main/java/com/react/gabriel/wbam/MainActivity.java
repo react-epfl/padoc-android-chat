06e73c81aba928a34593071c4938706388934616
GabGett <gabriel.detiberge@gmail.com>
2016-07-28 18:11:02 +0200

    PadocChat proof of concept

--- app/src/main/java/com/react/gabriel/wbam/MainActivity.java
+++ app/src/main/java/com/react/gabriel/wbam/MainActivity.java
@@ -1,24 +1,29 @@
 package com.react.gabriel.wbam;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.InputType;
 import android.text.Layout;
+import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.react.gabriel.wbam.padoc.Message;
-import com.react.gabriel.wbam.padoc.Messenger;
-import com.react.gabriel.wbam.padoc.PadocManager;
-import com.react.gabriel.wbam.padoc.TestThread;
+import com.react.gabriel.wbam.padoc.Padoc;
+import com.react.gabriel.wbam.padoc.PadocInterface;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -26,17 +31,15 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PadocInterface {
 
-    //DEBUG
-    private boolean DBG = true;
-    private static final String TAG = "WBAM";
     Date date = null;
     SimpleDateFormat dateFormat = null;
     private TextView console = null;
 
-    private PadocManager padocManager;
+    private Padoc padoc;
 
     AlertDialog peerDialog;
 
@@ -52,16 +55,8 @@ public class MainActivity extends AppCompatActivity {
         console = (TextView)findViewById(R.id.console);
         console.setMovementMethod(new ScrollingMovementMethod());
 
-        //PADOC
-        padocManager = new PadocManager(this);
+        padoc = new Padoc(this, getApplicationContext());
 
-    }
-
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-
-        padocManager.onDestroy();
     }
 
     @Override
@@ -86,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void showPeers(View view){
+    public void sendMessage(View view){
 
         LayoutInflater inflater = getLayoutInflater();
         View convertView = (View) inflater.inflate(R.layout.custom, null);
@@ -105,213 +100,172 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 HashMap<String, String> selectedPeer = (HashMap) lv.getItemAtPosition(position);
 
-                if(selectedPeer.get("addr").equals("ALL")){
-                    padocManager.sendCBS();
+                if(selectedPeer.get("addr").equals("FLOOD")){
+
+                    sendFLOOD();
+                    peerDialog.dismiss();
+                }else if(selectedPeer.get("addr").equals("CBS")){
+                    sendCBS();
                     peerDialog.dismiss();
                 }else {
-                    padocManager.sendMsg(selectedPeer.get("addr"));
+                    MainActivity.this.sendMessage(selectedPeer.get("addr"));
                     peerDialog.dismiss();
                 }
-
-
             }
         });
 
         List<Map<String, String>> data = new ArrayList<Map<String, String>>();
 
+        Map<String, String> floodEntry = new HashMap<>();
+        floodEntry.put("name", "FLOOD");
+        floodEntry.put("addr", "FLOOD");
+        data.add(floodEntry);
         Map<String, String> cbsEntry = new HashMap<>();
-        cbsEntry.put("name", "CBS (" + padocManager.getMeshUUID() + ")");
-        cbsEntry.put("addr", "ALL");
+        cbsEntry.put("name", "CBS");
+        cbsEntry.put("addr", "CBS");
         data.add(cbsEntry);
 
-        for (Map.Entry<String, String> peerItem : padocManager.getPeerNames()) {
+        Set peers = padoc.getPeers();
 
-            Map<String, String> datum = new HashMap<String, String>(2);
-            String addr = peerItem.getKey();
+        if(peers != null){
 
-            String nameAndHops = peerItem.getValue() + " ("+ padocManager.getHopsFor(addr) + ")";
-            datum.put("name", nameAndHops);
-            datum.put("addr", addr);
-            data.add(datum);
+            for (Map.Entry<String, String> peerItem : padoc.getPeers()) {
+
+                Map<String, String> datum = new HashMap<String, String>(2);
+                String addr = peerItem.getKey();
+
+                String nameAndHops = peerItem.getValue() + " ("+ padoc.getHopsFor(addr) + ")";
+                datum.put("name", nameAndHops);
+                datum.put("addr", addr);
+                data.add(datum);
+            }
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(this, data, android.R.layout.simple_list_item_2,
+                    new String[] {"name", "addr"}, new int[] {android.R.id.text1, android.R.id.text2});
+
+            lv.setAdapter(simpleAdapter);
+            peerDialog.show();
+        }else {
+            Toast.makeText(getApplicationContext(), "There is no one to chat with : (", Toast.LENGTH_SHORT).show();
         }
-
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, data, android.R.layout.simple_list_item_2,
-                new String[] {"name", "addr"}, new int[] {android.R.id.text1, android.R.id.text2});
-
-        lv.setAdapter(simpleAdapter);
-        peerDialog.show();
-
     }
 
-    public void startBTDiscovery(View view){
-//        padocManager.startBluetoothDiscovery();
+    private void sendFLOOD(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog dialog;
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_TEXT_FLAG_IME_MULTI_LINE);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                padoc.sendFLOOD(input.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        dialog = builder.create();
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        dialog.show();
     }
 
-    public void stopBTDiscovery(View view){
-//        padocManager.stopBluetoothDiscovery();
+    private void sendCBS(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog dialog;
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_TEXT_FLAG_IME_MULTI_LINE);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                padoc.sendCBS(input.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        dialog = builder.create();
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        dialog.show();
     }
 
-    public void makeBTVisible(View view){
-        padocManager.setBluetoothVisible();
+    private void sendMessage(final String destination){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog dialog;
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_TEXT_FLAG_IME_MULTI_LINE);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                padoc.sendMessage(destination, input.getText().toString());
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        dialog = builder.create();
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        dialog.show();
     }
 
-    public void makeBTInvisible(View view){
-        padocManager.setBluetoothInvisible();
-    }
+    @Override
+    public void print(final Spanned spannedHtml, final boolean time){
 
-    public void startBTServer(View view){
-        padocManager.startBluetoothServer();
-    }
-
-    public void unpairDevices(View view) {
-        padocManager.unpairBluetoothDevices();
-    }
-
-    public void startWDDiscovery(View view){
-        padocManager.startWifiDirectDiscovery();
-    }
-
-    public void stopWDDiscovery(View view){
-        padocManager.stopWifiDirectDiscovery();
-    }
-
-    public void makeWDVisible(View view){
-        padocManager.startWifiDirectService();
-    }
-
-    public void makeWDInvisible(View view){
-        padocManager.forceStopWifiDirectService();
-    }
-
-    public void initialize(View view){
-        padocManager.initialize();
-    }
-
-    public void startFLOODTestSlow(View view){
-        TestThread testThread = new TestThread(padocManager);
-        debugPrint("STARTING SLOW FLOOD");
-        testThread.startTest(Message.Algo.FLOOD, 2000, null);
-    }
-
-    public void startFLOODTestMedium(View view){
-        TestThread testThread = new TestThread(padocManager);
-        debugPrint("STARTING MEDIUM FLOOD");
-        testThread.startTest(Message.Algo.FLOOD, 1000, null);
-    }
-
-    public void startFLOODTestFast(View view){
-        TestThread testThread = new TestThread(padocManager);
-        debugPrint("STARTING FAST FLOOD");
-        testThread.startTest(Message.Algo.FLOOD, 500, null);
-    }
-
-    public void startCBSTestSlow(View view){
-        TestThread testThread = new TestThread(padocManager);
-        debugPrint("STARTING SLOW CBS");
-        testThread.startTest(Message.Algo.CBS, 2000, null);
-    }
-
-    public void startCBSTestMedium(View view){
-        TestThread testThread = new TestThread(padocManager);
-        debugPrint("STARTING SLOW CBS");
-        testThread.startTest(Message.Algo.CBS, 1000, null);
-    }
-
-    public void startCBSTestFast(View view){
-        TestThread testThread = new TestThread(padocManager);
-        debugPrint("STARTING SLOW CBS");
-        testThread.startTest(Message.Algo.CBS, 500, null);
-    }
-
-    public void requestFLOODTestSlow(View view){
-        padocManager.requestTest(Message.Type.FLOOD_TEST_REQUEST, 2000);
-        debugPrint("Request for SLOW FLOOD SENT");
-    }
-
-    public void requestFLOODTestMedium(View view){
-        padocManager.requestTest(Message.Type.FLOOD_TEST_REQUEST, 1000);
-        debugPrint("Request for MEDIUM FLOOD SENT");
-    }
-
-    public void requestFLOODTestFast(View view){
-        padocManager.requestTest(Message.Type.FLOOD_TEST_REQUEST, 500);
-        debugPrint("Request for FAST FLOOD SENT");
-    }
-
-    public void requestCBSTestSlow(View view){
-        padocManager.requestTest(Message.Type.CBS_TEST_REQUEST, 2000);
-        debugPrint("Request for SLOW CBS SENT");
-    }
-
-    public void requestCBSTestMedium(View view){
-        padocManager.requestTest(Message.Type.CBS_TEST_REQUEST, 1000);
-        debugPrint("Request for MEDIUM CBS SENT");
-    }
-
-    public void requestCBSTestFast(View view){
-        padocManager.requestTest(Message.Type.CBS_TEST_REQUEST, 500);
-        debugPrint("Request for FAST CBS SENT");
-    }
-
-    public void requestROUTETestSlow(View view){
-        padocManager.requestTest(Message.Type.ROUTE_TEST_REQUEST, 2000);
-        debugPrint("Request for SLOW ROUTE SENT");
-    }
-
-    public void requestROUTETestMedium(View view){
-        padocManager.requestTest(Message.Type.ROUTE_TEST_REQUEST, 1000);
-        debugPrint("Request for MEDIUM ROUTE SENT");
-    }
-
-    public void requestROUTETestFast(View view){
-        padocManager.requestTest(Message.Type.ROUTE_TEST_REQUEST, 500);
-        debugPrint("Request for FAST ROUTE SENT");
-    }
-
-    public void sendCBS(View view){
-        padocManager.sendCBS();
-    }
-
-    public void sendFLOOD(View view){
-        padocManager.sendFLOOD();
-    }
-
-    public void showMessageCount(View view){
-        padocManager.showTestResults();
-    }
-
-    public void clearMessageCount(View view){
-        padocManager.clearMessageCount();
-        console.setText("");
-        debugPrint("OK");
-    }
-
-    //DEBUG
-    public void debugPrint(final String msg){
-
-        if(DBG) {
-            //Log on Android Monitor
-            Log.d(MainActivity.TAG, msg);
-
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    //Log on the console output of the app
-                    console.append(dateFormat.format(new Date()) + " " + msg + "\n");
-                    console.post(new Runnable() {
-                        public void run() {
-                            Layout l = console.getLayout();
-                            if (l == null)
-                                return;
-                            final int scrollAmount = l.getLineTop(console.getLineCount()) - console.getHeight();
-                            if (scrollAmount > 0)
-                                console.scrollTo(0, scrollAmount);
-                            else
-                                console.scrollTo(0, 0);
-                        }
-                    });
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //Log on the console output of the app
+                if(time){
+                    console.append(TextUtils.concat(TextUtils.concat(Html.fromHtml("<i>" + dateFormat.format(new Date())+ ": </i>"), spannedHtml), "\n\n"));
+                }else{
+                   console.append(TextUtils.concat(spannedHtml, Html.fromHtml("\n\n")));
                 }
-            });
-        }
+
+                console.post(new Runnable() {
+                    public void run() {
+                        Layout l = console.getLayout();
+                        if (l == null)
+                            return;
+                        final int scrollAmount = l.getLineTop(console.getLineCount()) - console.getHeight();
+                        if (scrollAmount > 0)
+                            console.scrollTo(0, scrollAmount);
+                        else
+                            console.scrollTo(0, 0);
+                    }
+                });
+            }
+        });
     }
 }
